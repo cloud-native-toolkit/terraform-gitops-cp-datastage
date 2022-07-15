06e3d20e883fdf53b0d3eb53a89695448fe819e0
Vivek Pratap <vivprata@in.ibm.com>
2022-07-15 14:11:02 +0530

    Dev datastage (#1)
    * testing
    
    Signed-off-by: vrp-git <vivekpratap123@gmail.com>
    
    * DataStage service on Cloud Pak for Data
    
    * Created DataStage service on Cloud Pak for Data
    
    Signed-off-by: Vivek Pratap <vivekpratap123@gmail.com>
    
    * Created DataStage service on Cloud Pak for Data
    
    Signed-off-by: Vivek Pratap <vivekpratap123@gmail.com>
    
    * Created DataStage service on Cloud Pak for Data
    
    Signed-off-by: Vivek Pratap <vivprata@in.ibm.com>
    
    * DataStage service on Cloud Pak for Data
    
    Signed-off-by: Vivek Pratap <vivprata@in.ibm.com>
    
    * DataStage service on Cloud Pak for Data
    
    Signed-off-by: Vivek Pratap <vivprata@in.ibm.com>
    
    Co-authored-by: vrp-git <vivekpratap123@gmail.com>

--- test/stages/stage3-cpd-ds.tf
+++ test/stages/stage3-cpd-ds.tf
@@ -1,4 +1,4 @@
-module "gitops_module" {
+module "cp-datastage" {
   source = "./module"
 
   gitops_config = module.gitops.gitops_config
@@ -6,4 +6,9 @@ module "gitops_module" {
   server_name = module.gitops.server_name
   namespace = module.gitops_namespace.name
   kubeseal_cert = module.gitops.sealed_secrets_cert
+  operator_namespace= "cpd-operators"
+  # module.gitops_cp4d_operator.namespace
+  cpd_namespace = "cp4d"
+  # module.cp4d-instance.namespace
+  storage_class = "ocs-storagecluster-cephfs"
 }
